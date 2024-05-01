86c95577e878f42d4bbe48a556429f20ca67f1ec
chinmay768 <chinmaysahani07@gmail.com>
2024-05-01 10:28:32 +0530

    30

---
+++ SubstringWithConcatenationOfAllWords30.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class SubstringWithConcatenationOfAllWords30 {
+
+    public static List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<Integer>();
+
+        int len = words[0].length();
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+
+        for(int i = 0; i <= s.length() - len * words.length; i++ ){
+            Map<String, Integer> copyMap = new HashMap<String, Integer>(map);
+
+            for(int j = 0; j < words.length; j++){
+                String str = s.substring(i + j * len, i + j * len + len);
+
+                if(copyMap.containsKey(str)){
+                    int count = copyMap.get(str);
+                    if(count == 1)
+                        copyMap.remove(str);
+                    else
+                        copyMap.put(str, count - 1);
+
+                    if(copyMap.isEmpty()){
+                        ans.add(i);
+                        break;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        String s = "barfoothefoobarman";
+        String[] words = {"foo","bar"};
+
+        System.out.println(findSubstring(s, words));
+    }
+}
