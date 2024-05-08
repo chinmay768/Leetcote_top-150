76df8f3eed70d59c03bb3551c46df1e3b3b87e88
chinmay768 <chinmaysahani07@gmail.com>
2024-05-08 23:15:27 +0530

    383

---
+++ RansomeNote383.java
@@ -0,0 +1,27 @@
+public class RansomeNote383 {
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for(char ch : magazine.toCharArray()){
+            count[ch - 'a'] += 1;
+        }
+
+        for(char ch : ransomNote.toCharArray()){
+            count[ch - 'a']--;
+
+            if(count[ch - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String ransomNote = "a";
+        String magazine = "b";
+
+        System.out.println(canConstruct(ransomNote, magazine));
+    }
+}
